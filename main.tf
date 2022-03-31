fbec0d2d12c89db8d2a6856df30f321fca0d4b22
Maximiliano Vera <jvera@certuit.com>
2022-03-30 19:02:43 -0700

    Se creo una subred privada y pública con la red ya creada

--- main.tf
+++ main.tf
@@ -9,3 +9,16 @@ resource "google_compute_network" "custom-vpc" {
   name = "custom-vpc"
   auto_create_subnetworks = "false"
 }
+
+resource "google_compute_subnetwork" "public-subnet"{
+  name = "public-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  network = google_compute_network.custom-vpc.id
+}
+
+resource "google_compute_subnetwork" "private-subnet"{
+  name = "private-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  private_ip_google_access = true
+  network = google_compute_network.custom-vpc.id
+}
