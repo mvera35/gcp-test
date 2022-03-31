b1f04852fe6b26bb6442073ebf3747308833b8b9
Maximiliano Vera <jvera@certuit.com>
2022-03-30 20:36:46 -0700

    Creación de una plantilla para un compute instance y creación de una compute instace a partir de la plantilla

--- main.tf
+++ main.tf
@@ -23,25 +23,35 @@ resource "google_compute_subnetwork" "private-subnet"{
   network = google_compute_network.custom-vpc.id
 }
 
-
-resource "google_compute_instance" "public-server" {
-  name         = "public-server-1"
+resource "google_compute_instance_template" "server-template" {
+  name = "server-template"
+  description = "Plantilla usada para montar servidores sencillos"
   machine_type = "f1-micro"
 
-  tags = ["public-1"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      size = 10
-    }
+  disk {
+    source_image = "debian-cloud/debian-11"
+    auto_delete = true
+    disk_size_gb = 10
+    boot = true
   }
+
+  network_interface {
+    network = "custom-vpc"
+    subnetwork = "private-subnet"
+  }
+
+  depends_on = [google_compute_subnetwork.private-subnet, google_compute_subnetwork.public-subnet]
+}
+
+resource "google_compute_instance_from_template" "public-server-1" {
+  name = "public-server-1"
+
+  source_instance_template = google_compute_instance_template.server-template.id
 
   network_interface {
     network = "custom-vpc"
     subnetwork = "public-subnet"
   }
 
-  depends_on = [google_compute_subnetwork.public-subnet]
-
+  depends_on = [google_compute_instance_template.server-template]
 }
