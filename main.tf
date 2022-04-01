c2608406529afa2398fa39362135afcaf24c34a9
Maximiliano Vera <jvera@certuit.com>
2022-03-31 20:46:25 -0700

    Se agrego un firewall, una ip estática y una llave pública ssh al template del server

--- main.tf
+++ main.tf
@@ -10,6 +10,24 @@ resource "google_compute_network" "custom-vpc" {
   auto_create_subnetworks = "false"
 }
 
+resource "google_compute_firewall" "firewall" {
+  name    = "firewall-externalssh"
+  network = "custom-vpc"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]   
+  target_tags   = ["externalssh"]
+  depends_on = [google_compute_network.custom-vpc]
+}
+
+resource "google_compute_address" "static" {
+  name = "public-address"
+  project = file("./project-id.txt")
+  depends_on = [ google_compute_firewall.firewall ]
+}
+
 resource "google_compute_subnetwork" "public-subnet"{
   name = "public-subnet"
   ip_cidr_range = "10.0.1.0/24"
@@ -23,34 +41,18 @@ resource "google_compute_subnetwork" "private-subnet"{
   network = google_compute_network.custom-vpc.id
 }
 
-resource "google_compute_instance_template" "server-template" {
-  name = "server-template"
-  description = "Plantilla usada para montar servidores sencillos"
-  machine_type = "f1-micro"
-
-  disk {
-    source_image = "debian-cloud/debian-11"
-    auto_delete = true
-    disk_size_gb = 10
-    boot = true
-  }
-
-  network_interface {
-    network = "custom-vpc"
-    subnetwork = "private-subnet"
-  }
-
-  depends_on = [google_compute_subnetwork.private-subnet, google_compute_subnetwork.public-subnet]
-}
-
 resource "google_compute_instance_from_template" "public-server-1" {
   name = "public-server-1"
+  hostname = "gcp-test.com"
 
   source_instance_template = google_compute_instance_template.server-template.id
 
   network_interface {
     network = "custom-vpc"
     subnetwork = "public-subnet"
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
   }
 
   depends_on = [google_compute_instance_template.server-template]
@@ -66,5 +68,31 @@ resource "google_compute_instance_group_manager" "private-servers" {
 
    target_size = 2
    depends_on = [google_compute_instance_template.server-template]
+
+}
+
+resource "google_compute_instance_template" "server-template" {
+  name = "server-template"
+  description = "Plantilla usada para montar servidores sencillos"
+  machine_type = "f1-micro"
+  tags = ["externalssh"]
+
+  disk {
+    source_image = "debian-cloud/debian-11"
+    auto_delete = true
+    disk_size_gb = 10
+    boot = true
+  }
+
+  network_interface {
+    network = "custom-vpc"
+    subnetwork = "private-subnet"
+  }
+
+  depends_on = [google_compute_subnetwork.private-subnet, google_compute_subnetwork.public-subnet]
+
+  metadata = {
+    ssh-keys = "mvera:${file("/home/mvera/.ssh/id_rsa.pub")}"
+  }
 
 }
